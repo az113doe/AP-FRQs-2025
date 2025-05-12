1212d7833176c63fb5c892c40369b95cdcd1423d
az113doe <andyz113@nycstudents.net>
2025-05-12 10:10:32 -0400

    Update Part(a).java

--- Q1/Part(a).java
+++ Q1/Part(a).java
@@ -1,3 +1,10 @@
 public int walkDogs(int hour) {
-
+  int num = company.numAvaliableDogs(hour);
+  if (maxDogs >= num) {
+      company.updateDogs(hour, num);
+      return num;
+  }else {
+      company.updateDogs(hour, maxDogs):
+      return maxDogs;
+  }
 }
