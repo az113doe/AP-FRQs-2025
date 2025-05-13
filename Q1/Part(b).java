8a74c977995c8b8d6362f41f7f522f7df577d77f
az113doe <andyz113@nycstudents.net>
2025-05-13 09:58:53 -0400

    Update Part(b).java

--- Q1/Part(b).java
+++ Q1/Part(b).java
@@ -1,14 +1,13 @@
 public int dogWalkShift(int startHour, int endHour) {
-  int earn = 0;
-  for (int i = startHour; i <= endHour; i++) {
-    int walkedDogs = walkDogs(hour);
-    int earning = walkedDogs * 5;
+    int earn = 0;
+    for (int i = startHour; i <= endHour; i++) {
+        int walkedDogs = walkDogs(hour);
+        int earning = walkedDogs * 5;
     
-    if ((walkedDogs == maxDogs) || (hour >= 9 && hour <= 17)) {
-      earn += 3;
+        if ((walkedDogs == maxDogs) || (hour >= 9 && hour <= 17)) {
+            earn += 3;
+        }
+        earn += earning;
     }
-
-    earn += earning;
-  }
-  return earn;
+    return earn;
 }
