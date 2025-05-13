3654054ea6a94fcdda439968f75996efb22c5b04
az113doe <andyz113@nycstudents.net>
2025-05-13 09:58:06 -0400

    Update Part(a).java

--- Q1/Part(a).java
+++ Q1/Part(a).java
@@ -1,6 +1,6 @@
 public int walkDogs(int hour) {
-  int dogAv = company.numAvailableDogs(hour);
-  int dogsToWalk = Math.min(dogAv, maxDogs);
-  company.updateDogs(hour, dogsToWalk);
-  return dogsToWalk;
+    int dogAv = company.numAvailableDogs(hour);
+    int dogsToWalk = Math.min(dogAv, maxDogs);
+    company.updateDogs(hour, dogsToWalk);
+    return dogsToWalk;
 }
