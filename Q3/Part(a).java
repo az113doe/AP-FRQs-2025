ac69011a605af91ebd6be7a3e4c1af18670aaad3
az113doe <andyz113@nycstudents.net>
2025-05-13 15:58:44 -0400

    Update Part(a).java

--- Q3/Part(a).java
+++ Q3/Part(a).java
@@ -1,3 +1,7 @@
 public Round(String[] names) {
-
+    competitorList = new ArrayList<Competitor>();
+    for (int i = 0; i < names.length; i++) {
+        Competitor c = new Competitor(names[i], i + 1);
+        competitorList.add(c);
+    }
 }
