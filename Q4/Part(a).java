cf1266039ff992949e750595f109a22f2c34e554
az113doe <andyz113@nycstudents.net>
2025-05-13 16:07:50 -0400

    Update Part(a).java

--- Q4/Part(a).java
+++ Q4/Part(a).java
@@ -1,3 +1,8 @@
 public SumOrSameGame(int numRows, int numCols) {
-
+    puzzle = new int[numRows][numCols];
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numCols; j++) {
+            puzzle[i][c] = (int)(Math.random() * 9) + 1'
+        }
+    }
 }
