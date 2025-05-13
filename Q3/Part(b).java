94315f85498a99053964d89307aa6a2a2b40b08f
az113doe <andyz113@nycstudents.net>
2025-05-13 16:03:05 -0400

    Update Part(b).java

--- Q3/Part(b).java
+++ Q3/Part(b).java
@@ -1,3 +1,15 @@
 public ArrayList<Match> buildMatches() {
+    ArrayList<Match> matches = new ArrayList<Match>();
+    int left = 0;
+    int right = competitorList.size();
 
+    while (left < right) {
+        Competitor highRank = competitorList.get(left);
+        Competitor lowRank = competitorList.get(right);
+        Match m = new Match(highRank, lowRank);
+        matches.add(m);
+        left++;
+        right--;
+    }
+    return matches;
 }
