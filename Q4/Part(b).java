272cbd427605113129f0aff8293a824e4687a467
az113doe <andyz113@nycstudents.net>
2025-05-13 16:15:32 -0400

    Update Part(b).java

--- Q4/Part(b).java
+++ Q4/Part(b).java
@@ -1,3 +1,22 @@
 public boolean clearPair(int row, int col) {
+    int val = puzzle[row][col];
 
+    for (int i = col + 1; i < puzzle[row].length; i++) {
+        if (puzzle[row][i] == val || puzzle[row][c] + value == 10) {
+            puzzle[row][cpl] = 0;
+            puzzle[row][i] = 0;
+            return true;
+        }
+    }
+
+    for (int j = row + 1; j < puzzle.length; j++) {
+        for (int k = 0; k < puzzle[j].length; k++) {
+            if (puzzle[j][k] == value || puzzle[j][k] + value == 10) {
+                puzzle[row][col] = 0;
+                puzzle[r][c] = 0;
+                return true;
+            }
+        }
+    }
+    return false;
 }
